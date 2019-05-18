2a75bb368c864965306658e8477af579887bfdc9
Olegario Castellanos Guzmán <olegario.castellanos@gmail.com>
2019-05-17 21:15:31 -0500

    Add files via upload
    Se agrega cliente para invocacion de servicios web sin el uso de un wsdl

---
+++ cliente_calculadora_soap_non_wsdl.php
@@ -0,0 +1,56 @@
+<?php
+	//Define el modelo para el request
+	class Add
+	{
+	   public $intA;
+	   public $intB;
+	}
+	
+	//Crea la instancia de la clase y se asignan valores a los atributos
+	$add = new Add();
+	$add->intA = 10;
+	$add->intB = 5;
+	
+	//Definen las opciones para invocacion
+	$options = array(
+			'location' => "http://www.dneonline.com/calculator.asmx",
+			'uri'=> "http://tempuri.org/",
+			'soap_version'=>SOAP_1_2,
+			'exceptions'=>true,
+			'trace'=>1,
+			'cache_wsdl'=>WSDL_CACHE_NONE
+	);
+	
+	try
+	{
+		//Inicializando el cliente
+		$client = new SoapClient(NULL, $options);
+		
+		//Se especifican los parametros a enviar y se invoca al WS SOAP
+		$resp = $client->__soapCall("Add",
+			array(new SoapParam(new SoapVar('10', XSD_INTEGER), 'ns1:intA'),
+			      new SoapParam(new SoapVar('5', XSD_INTEGER), 'ns1:intB')
+			),
+			array('soapaction' => 'http://tempuri.org/Add')
+		);
+		
+		//Invoca al WS SOAP
+		//$resp = $client->Add($add);
+		
+		//Imprimir los request & response
+		echo "REQUEST: ".htmlentities($client->__getLastRequest())."<br><br>";
+		
+		echo "RESPONSE: ".htmlentities($client->__getLastResponse());
+		
+		//Imprimiendo el objeto
+		var_dump($resp);
+		
+		//Imprimir el valor
+		echo "Imprimir Valor: ".$resp;
+		//echo "Imprimir Valor: ".$resp->AddResult;
+		
+	}catch(Exception $e)
+	{
+		echo "<b>Error: </b>".$e->getMessage();
+	}	
+?>
